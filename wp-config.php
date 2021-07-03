b276856bfd8a7443713acbdbdf2a090dcf1bfda6
anirban das <anirban.techlead@gmail.com>
2021-07-03 21:51:19 +0530

    config update

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'myblog' );
+define( 'DB_NAME', 'affliate_wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'phpmyadmin' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '#Sabuj123456' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
